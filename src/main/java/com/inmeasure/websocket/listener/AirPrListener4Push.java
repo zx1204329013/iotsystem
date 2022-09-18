2f50372d711050f87e792e83f4592a708d852c30
zhangxu <1204329013@qq.com>
2022-09-18 16:59:41 +0800

    完成接收气压传感器数据推送至前端功能

---
+++ src/main/java/com/inmeasure/websocket/listener/AirPrListener4Push.java
@@ -0,0 +1,32 @@
+package com.inmeasure.websocket.listener;
+
+import com.inmeasure.common.event.AirprEvent;
+import com.inmeasure.common.event.TempAndHumEvent;
+import com.inmeasure.emqx.domain.AirPr;
+import com.inmeasure.emqx.domain.TempAndHum;
+import com.inmeasure.websocket.service.AirPrWebSocketService;
+import com.inmeasure.websocket.service.TempHumWebSocketService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+/**
+ * 气压传感器推送消息监听器
+ */
+@Component
+@Slf4j
+public class AirPrListener4Push implements ApplicationListener<AirprEvent> {
+    /**
+     * 推送消息方法
+     * @param event
+     */
+    @Override
+    @Async("taskExecutor")
+    public void onApplicationEvent(AirprEvent event) {
+        AirPr airPr = event.getAirPr();
+        String s = airPr.toString();
+        log.info("气压传感器器消息推送监听器：" + s);
+        AirPrWebSocketService.sendInfo(airPr);
+    }
+}
